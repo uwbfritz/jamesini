222a0fe5059ad47b2f8c89e8536fbb336926b4b8
Bill <bfritz@uw.edu>
2023-01-23 11:16:03 -0800

    script move

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -6,6 +6,6 @@ if [ -z "$1" ]
     echo "No SQL file supplied"
     exit 1
 else
-  docker cp /tmp/james/$1 "$(docker ps -aqf "name=drupal9_jamesini_frontend")":/tmp/$1
+  docker cp $1 "$(docker ps -aqf "name=drupal9_jamesini_frontend")":/tmp/$1
   docker exec -it "$(docker ps -aqf "name=drupal9_jamesini_frontend")" /usr/bin/mysql -uroot -pjamesini* -hdb drupal9_jamesini -e 'source /tmp/'$1''
 fi
